af9745bead1cbc85c5963c3fdbf2c92ba8834a07
ubaidullaevv <ubaiduloevh@gmail.com>
2025-01-03 17:40:35 +0500

    Home Task Commit

---
+++ Domain/Students.cs
@@ -0,0 +1,13 @@
+namespace Domain;
+
+public class Student
+{
+public int Id { get; set; }
+public string? FirstName { get; set; }
+public string? LastName { get; set; }
+public int Age { get; set; }
+public string? Gender { get; set; }
+public string? Status { get; set; }
+public DateTime DateOfStart { get; set; }
+public DateTime DateOfFinish { get; set; }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Domain;
+// ClASS TASK
+
+
 List<Employee> employees=new List<Employee>()
     {
         new Employee{Age=18,Email="jhgfd@hgfghj.com",Experience=2,FirstName="John",LastName="DFGH",Id=1,Salary=2209},
@@ -71,4 +74,127 @@ List<Employee> employees=new List<Employee>()
 // {
 //  System.Console.WriteLine(item.FirstName);   
 // }
+
+
+
+
+
+
+// --------------------------HOME TASK ------------------------
+
+List<Student> students = new List<Student>()
+{
+    new Student{Age=18,FirstName="John",LastName="Johnson",Gender="Male",Id=1,Status="Active",DateOfFinish=DateTime.Now},
+    new Student{Age=18,FirstName="Ali",LastName="Sharipov",Gender="Male",Id=1,Status="Active",DateOfFinish=DateTime.Now},
+    new Student{Age=18,FirstName="Vali",LastName="Kodirov",Gender="Male",Id=1,Status="Active",DateOfFinish=DateTime.Now},
+    new Student{Age=18,FirstName="Mahmud",LastName="John",Gender="Male",Id=1,Status="Active",DateOfFinish=DateTime.Now},
+    new Student{Age=18,FirstName="Abu",LastName="Aliev",Gender="Female",Id=1,Status="Active",DateOfFinish=DateTime.Now},
+    new Student{Age=18,FirstName="Sabina",LastName="Arabova",Gender="Female",Id=1,Status="Active",DateOfFinish=DateTime.Now}
+};
+
+
+
+// 1----------------------------------------------------------------
+
+
+// var std=students.Where(s=>s.Gender=="Male").ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+// }
+
+// 2----------------------------------------------------------------
+
+
+// var std=students.Where(s=>s.Status=="InActive").ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+// }
+
+
+// 3----------------------------------------------------------------
+
+// var std=students.Where(s=>s.Age>=24).ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+// }
+
+// 4----------------------------------------------------------------
+
+// var std=students.Where(s=>s.Age==20 && s.Status=="Active").ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+// }
+
+
+// 5----------------------------------------------------------------
+
+
+// var std=students.Where(s=>(s.DateOfStart.Year-(2025-s.Age))>16).ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+// }
+
+
+// 6----------------------------------------------------------------
+
+// var std=students.Where(s=>(s.DateOfStart.Year-(2025-s.Age))>=18).ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+// }
+
+
+// 7 ------------------------------------------------------------------------
+
+// var std=students.Where(s=>s.FirstName.StartsWith("A") || s.FirstName.StartsWith("I")).ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+// }
+
+// 8 ------------------------------------------------------------------------
+
+// var std=students.Where(s=>s.FirstName[0]==s.LastName[0]).ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+//     System.Console.WriteLine(item.LastName);
+// }
+
+
+
+// 9 ------------------------------------------------------------------------
+
+// var std=students.Where(s => s.DateOfFinish.Year-s.DateOfStart.Year+s.Age >=18).ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+//     System.Console.WriteLine(item.LastName);
+// }
+
+
+// 10 ------------------------------------------------------------------------
+
+// var std=students.Where(s=>s.Gender!="Female" && s.DateOfStart.Year-(2025-s.Age)>16 && 2025- s.DateOfStart.Year>2).ToList();
+
+// foreach (var item in std)
+// {
+//     System.Console.WriteLine(item.FirstName);
+//     System.Console.WriteLine(item.LastName);
+// }
+
 
